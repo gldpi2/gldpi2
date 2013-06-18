cdad0a0d73b1294b886bf3819a2f87bde2bfcb18
wjs <wjs@certi.org.br>
2013-06-18 11:41:34 -0300

    Implementando novos testes no gráfico.

--- GLD/gld/src/view/GraphTest.java
+++ GLD/gld/src/view/GraphTest.java
@@ -1,76 +1,103 @@
 package view;
 
+import java.awt.Color;
+import java.awt.Font;
 import javax.swing.JFrame;
 
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
+import org.jfree.chart.annotations.XYPointerAnnotation;
+import org.jfree.chart.axis.ValueAxis;
 import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.plot.XYPlot;
 import org.jfree.data.xy.XYSeries;
 import org.jfree.data.xy.XYSeriesCollection;
+import org.jfree.ui.TextAnchor;
 
 public class GraphTest extends JFrame {
 
-    XYSeries series = new XYSeries("XYGraph");
+    XYSeries series = new XYSeries("Carga kW");
     private static final long serialVersionUID = 1L;
+    int count = 0;
 
     public GraphTest(String applicationTitle, String chartTitle) {
         super(applicationTitle);
 
-        // based on the dataset we create the chart
         JFreeChart chart = createChart();
-        // we put the chart into a panel
-        ChartPanel chartPanel = new ChartPanel(chart);
-        // default size
-        chartPanel.setPreferredSize(new java.awt.Dimension(500, 270));
-        // add it to our application
-        setContentPane(chartPanel);
 
+        ChartPanel chartPanel = new ChartPanel(chart);
+
+        chartPanel.setPreferredSize(new java.awt.Dimension(500, 270));
+
+        setContentPane(chartPanel);
     }
 
     private JFreeChart createChart() {
         series = new XYSeries("XYGraph");
 
-
-        // Add the series to your data set
         XYSeriesCollection dataset = new XYSeriesCollection();
         dataset.addSeries(series);
 
-        JFreeChart chart = ChartFactory.createXYAreaChart("graph", "Potência Ativa", "Hora", dataset, PlotOrientation.VERTICAL, true, true, false);
+        JFreeChart chart = ChartFactory.createXYAreaChart("Curva de Carga", "Hora", "Potência Ativa", dataset, PlotOrientation.VERTICAL, true, true, false);
+
+        XYPlot xyplot = (XYPlot) chart.getPlot();
+        xyplot.setBackgroundPaint(Color.lightGray);
+        xyplot.setForegroundAlpha(0.65F);
+        xyplot.setDomainGridlinePaint(Color.white);
+        xyplot.setRangeGridlinePaint(Color.white);
+
+        ValueAxis xAxis = xyplot.getDomainAxis();
+
+        xAxis.setTickMarkPaint(Color.black);
+        xAxis.setRange(0, 100);
+
+        ValueAxis yAxis = xyplot.getRangeAxis();
+        yAxis.setRange(0, 100);
+
+        XYPointerAnnotation xypointerannotation = new XYPointerAnnotation("Test", 5D, -500D, 2.3561944901923448D);
+        xypointerannotation.setTipRadius(0.0D);
+        xypointerannotation.setBaseRadius(35D);
+        xypointerannotation.setFont(new Font("SansSerif", 0, 9));
+        xypointerannotation.setPaint(Color.blue);
+        xypointerannotation.setTextAnchor(TextAnchor.HALF_ASCENT_RIGHT);
+        xyplot.addAnnotation(xypointerannotation);
 
         return chart;
-
     }
 
     public static void main(String[] args) {
-        GraphTest graph = new GraphTest("Comparison", "Which operating system are you using?");
+        GraphTest graph = new GraphTest("Comparison", "Curva de Carga");
         graph.pack();
 
-        Thread th = graph.new UpdaterThread();
+        Thread th = new Thread(graph.new UpdaterThread());
         th.setDaemon(true);
         th.start();
-        
+
         graph.setVisible(true);
+
     }
 
-    private class UpdaterThread extends Thread {
+    private class UpdaterThread implements Runnable {
+
         private double SIZE = 100;
 
         @Override
         public void run() {
-            setPriority(MIN_PRIORITY); 
 
             int i = 1;
             while (true) {
                 i++;
+
                 final int j = (int) (Math.random() * SIZE);
 
-                series.addOrUpdate(i, j);
+                if (i % 2 == 0) {
+                    series.addOrUpdate(i, j);
+                }
 
                 try {
-                    Thread.sleep(500);
+                    Thread.sleep(1000);
                 } catch (InterruptedException e) {
-                    
                 }
             }
         }
