6e3333704a301c28d06b80ccbcaeead7e493c16a
Matheus Freire <matheus.sfreire@gmail.com>
2013-06-19 13:27:32 -0300

    Adicao da Model e DAO de Custo e renomeacao do grafico

---
+++ GLD/gld/src/model/Cost.java
@@ -0,0 +1,34 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Matheus
+ */
+public class Cost {
+    double flow;
+    double tension;
+
+    public double getFlow() {
+        return flow;
+    }
+
+    public void setFlow(double flow) {
+        this.flow = flow;
+    }
+
+    public double getTension() {
+        return tension;
+    }
+
+    public void setTension(double tension) {
+        this.tension = tension;
+    }
+     
+    
+    
+    
+}
