cd7e512b16de97c46a3a5fca65e1e8dbc0cacc53
Fernando <fernandosouzs@outlook.com>
2013-07-02 00:27:37 -0300

    Classe de teste da classe EstimationOnHistory

---
+++ GLD/gld/test/model/EstimationOnHistoryModelTest.java
@@ -0,0 +1,52 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Fernando
+ */
+public class EstimationOnHistoryModelTest {
+    
+    public EstimationOnHistoryModelTest() {
+    }
+    
+    @Test
+    public void estimar(){
+        
+        EstimationOnHistory est = new EstimationOnHistory();
+        double resultado = est.pert(5, 2, 3);
+        
+        assertEquals(3.1, resultado);
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
