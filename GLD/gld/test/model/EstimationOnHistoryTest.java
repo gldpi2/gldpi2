e11a13c1cd60f40cd8c33b9c66dab52018b34d7f
Fernando <fernandosouzs@outlook.com>
2013-07-02 00:16:14 -0300

    Teste da classe EstimationOnHistory

---
+++ GLD/gld/test/model/EstimationOnHistoryTest.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Fernando
+ */
+public class EstimationOnHistoryTest {
+    
+    public EstimationOnHistoryTest(){
+        
+    }
+    
+}
