b745449778a4c6b13393fe29c929f8f0c170f95c
Fernando <fernandosouzs@outlook.com>
2013-07-01 23:23:38 -0300

    Implementação da classe controladora de estimativas

---
+++ GLD/gld/src/controller/EstimationCtrl.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+/**
+ *
+ * @author Fernando
+ */
+public class EstimationCtrl {
+    
+}
