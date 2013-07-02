5717becf5f5d0f39b903c68cc8f42625178dec3e
Fernando <fernandosouzs@outlook.com>
2013-07-02 00:14:38 -0300

    função PERT implementada!

--- GLD/gld/src/model/EstimationOnHistory.java
+++ GLD/gld/src/model/EstimationOnHistory.java
@@ -10,4 +10,22 @@ package model;
  */
 public class EstimationOnHistory {
     
+    private double index;
+    
+    public double pert(double greater, double minor, double better){
+        
+        index = ((greater + minor + (4*better))/6);
+        
+        return index;
+        
+    }    
+    
+    public static void main(String args[]){
+            System.out.println("Indice");
+            
+            EstimationOnHistory est = new EstimationOnHistory();
+            
+            System.out.println("Resultado: "+est.pert(300,20,130));
+            
+        }
 }
