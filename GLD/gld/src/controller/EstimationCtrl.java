8195254036a231d7314ebbe50a82738c6cc73349
Fernando <fernandosouzs@outlook.com>
2013-07-02 02:02:30 -0300

    Refatoração

--- GLD/gld/src/controller/EstimationCtrl.java
+++ GLD/gld/src/controller/EstimationCtrl.java
@@ -4,10 +4,41 @@
  */
 package controller;
 
+import model.EstimationOnHistory;
+
 /**
  *
  * @author Fernando
  */
 public class EstimationCtrl {
     
+    EstimationOnHistory est = new EstimationOnHistory();
+    String minor;
+    double greater;
+    double better;
+    private double minorIndex;
+
+    public String getMinor() {
+        return minor;
+    }
+
+    public void setMinor(String minor) {
+        this.minor = minor;
+    }
+
+    public double getGreater() {
+        return greater;
+    }
+
+    public void setGreater(double greater) {
+        this.greater = greater;
+    }
+
+    public double getBetter() {
+        return better;
+    }
+
+    public void setBetter(double better) {
+        this.better = better;
+    }    
 }
