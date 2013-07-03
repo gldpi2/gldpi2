4e23616a7cc0ca91d6760157ac5c7300661e468f
Fernando <fernandosouzs@outlook.com>
2013-07-03 12:49:44 -0300

    Função que pega as medições das últimas 168 horas

--- GLD/gld/src/dao/EstimationOnHistoryDAO.java
+++ GLD/gld/src/dao/EstimationOnHistoryDAO.java
@@ -4,10 +4,48 @@
  */
 package dao;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import model.Mensuration;
+import utils.DatabaseInterface;
+
 /**
  *
  * @author Fernando
  */
 public class EstimationOnHistoryDAO {
+    
+    private final int NUMBER_REG_WEEK = 604800;
+    private final int NUMBER_REG_DAY = 86400;
+    private final int NUMBER_REG_HOUR = 3600;
+    private DatabaseInterface dbInterface = new DatabaseInterface();
+
+    public List<Mensuration> getMensurationLast168Hours() throws SQLException {
+        List<Mensuration> measurementList;
+
+        measurementList = new ArrayList<>();
+
+        String sql = "SELECT * "
+                + "FROM mensuration "
+                + "LIMITE 0, " + String.valueOf(NUMBER_REG_WEEK)
+                + "ORDER BY  `mensuration`.`timestamp` DESC ";
+
+        dbInterface.connect();
+        ResultSet rs = dbInterface.executeQuery(sql);
+        while (rs.next()) {
+            Mensuration mensuration;
+            mensuration = new Mensuration();
+            mensuration.setIdMensuration(rs.getInt("id_mensuration"));
+            mensuration.setFlow(rs.getDouble("flow"));
+            mensuration.setTension(rs.getDouble("tension"));
+            mensuration.setTimestamp(rs.getString("timestamp"));
+            measurementList.add(mensuration);
+        }
+        dbInterface.disconnect();
+        return measurementList;
+    }
+
     
 }
