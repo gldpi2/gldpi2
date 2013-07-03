f49635a110836ad258330a2e9e8735ebc0eed960
Fernando <fernandosouzs@outlook.com>
2013-07-03 13:02:43 -0300

    Criação da classe CostEstimationOnHistory

---
+++ GLD/gld/src/controller/CostEstimationOnHistoryCtrl.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+/**
+ *
+ * @author Fernando
+ */
+public class CostEstimationOnHistoryCtrl {
+
+    public void setTime(String substring) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public double energyValue() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
