5fd1dbd2789f0ede48c837f23722cc63810e8f62
Matheus Freire <matheus.sfreire@gmail.com>
2013-07-16 02:42:09 -0300

    Valores da energia alterado

--- GLD/gld/src/controller/CostCtrl.java
+++ GLD/gld/src/controller/CostCtrl.java
@@ -4,9 +4,6 @@
  */
 package controller;
 
-import static controller.CostCtrl.HOUR;
-import static controller.CostCtrl.PEAK;
-import static controller.CostCtrl.VALUE_OFFPEAK;
 import dao.CostDAO;
 import java.util.List;
 import model.Cost;
@@ -20,9 +17,11 @@ import org.jfree.data.time.TimeSeries;
  */
 public class CostCtrl {
 
-    public final static double HOUR = 3600;
-    public final static double PEAK = 19.65 / HOUR;
-    public final static double VALUE_OFFPEAK = 5.22 / HOUR;
+    private final static double PEAK = 0.3260544; 
+    private final static double VALUE_OFFPEAK = 0.2079277;
+    private final static double DEMANDPEAK = 19.65;
+    private final static double DEMANDOFFPEAK = 5.22;
+    
     private CostDAO cDao = new CostDAO();
     private List<Mensuration> listMensuration;
     private Cost cost = new Cost();
